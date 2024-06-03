0de29933b077a30de84483675d80d4bf450a5403
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-06-03 22:17:15 +0200

    setting up the models func to get the data from the json

--- models/models.go
+++ models/models.go
@@ -1,1 +1,63 @@
 package models
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Promos struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	GitHub string `json:"github_organization"`
+}
+
+type Students struct {
+	ID                int    `json:"id"`
+	Promo             string `json:"promo"`
+	FirstName         string `json:"first_name"`
+	LastName          string `json:"last_name"`
+	ProfilePictureUrl string `json:"profile_picture_url"`
+}
+
+func GetPromos(filename string) ([]Promos, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %w", err)
+	}
+	defer file.Close()
+
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	var promos []Promos
+	err = json.Unmarshal(bytes, &promos)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling JSON: %w", err)
+	}
+
+	return promos, nil
+}
+
+func GetStudents(filename string) ([]Students, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %w", err)
+	}
+	defer file.Close()
+
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	var students []Students
+	err = json.Unmarshal(bytes, &students)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling JSON: %w", err)
+	}
+
+	return students, nil
+}
